0d65fbe328ea47b70f85d25ba7b0729b211f5435
mimu <akito.mimura@nijibox.co.jp>
2023-10-24 14:25:39 +0900

    add

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+const hello = (message) => {
+    console.log(`hello ${message}`);
+};
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
